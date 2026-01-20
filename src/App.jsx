55c98ab9c55de6e6f192d8e13f177a05966f15d6
xettriabi6 <abichhetri75@gmail.com>
2026-01-20 22:06:11 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className='h-screen w-screen flex items-center justify-center text-white text-2xl'>
+      hello world
+    </div>
+  )
+}
+
+export default App
